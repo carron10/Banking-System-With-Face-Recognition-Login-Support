c43b499d2245e089db6673782516df22244ba509
Carron Muleya <carronmuleya10@gmail.com>
2024-05-31 15:35:54 +0200

    fixed login attempt issue

--- utils/detect_login_attempt.php
+++ utils/detect_login_attempt.php
@@ -1,11 +1,13 @@
 <?php
 
 include_once(__DIR__ . '/../client/conf/config.php');
+include_once(__DIR__ . '/utils.php');
+
 $client_id = null;
 
 function validateLoginCredentials(string $email, string $password): bool
 {
-    global $mysqli,$client_id; // Assuming a global connection is necessary
+    global $mysqli, $client_id; // Assuming a global connection is necessary
 
     if (!$mysqli instanceof mysqli) {
         throw new InvalidArgumentException('Invalid database connection provided.');
@@ -19,7 +21,7 @@ function validateLoginCredentials(string $email, string $password): bool
 
         $stmt->bind_param('s', $email);
         $stmt->execute();
-        $stmt->bind_result($db_email, $db_password_hash,$client_id);
+        $stmt->bind_result($db_email, $db_password_hash, $client_id);
         $stmt->fetch();
 
         if (!$db_email) {
@@ -43,18 +45,26 @@ function login($email, $password)
 {
     global $err, $client_id;
     $max_attempts = 3;  // Set the maximum allowed login attempts
-
-    $r = get_login_retrials($email);
-    if ($r > $max_attempts) {
+    $user_exist = check_if_user_exist($email);
+    if ($user_exist) {
+        $r = get_login_retrials($email);
+        if ($r > $max_attempts) {
 ?>
-        <script>
-            setTimeout(() => {
-                swal("Login Failed!!", "You have reached your maximum login attempts!!", 'error')
-                window.location.href = "/client/pages_client_index.php";
-            }, 2000)
-        </script>
+            <script>
+                setTimeout(() => {
+                    swal({
+                        title: 'Login Failed!!',
+                        text: 'You have reached your maximum login attempts!!',
+                        buttons: false,
+                        icon: 'error',
+                        allowOutsideClick:false
+                    })
+                    // window.location.href = "/client/pages_client_index.php";
+                }, 1000)
+            </script>
         <?php
-        exit();
+            return false;
+        }
     }
     // Login validation logic here (replace with your authentication code)
     if (validateLoginCredentials($email, $password)) {
@@ -80,31 +90,28 @@ function login($email, $password)
             header("location:pages_dashboard.php");
         }
     } else {
-
-        update_login_retrials($email, $r);
-        if ($r >= $max_attempts) {
-            $uuid = uniqid("", true);
-            update_token($email, $uuid);
-            $user_email = $email;  // Replace with actual data retrieval
-            send_login_retrial_link($user_email, $_SERVER['REMOTE_ADDR'] . "/client/api/activate_account.php?token=$uuid");
-            // Optionally, lock the account or display a message to the user
-            $err = "Your account have been temporary, you have reached the number of trials, Please check your email, we have sent your the activation code";
-        ?>
-            <script>
-                setTimeout(() => {
-                    window.location.href = "/client/pages_client_index.php";
-                }, 2000)
-            </script>
-        <?php
+        if ($user_exist) {
+            update_login_retrials($email, $r + 1);
+            if ($r >= $max_attempts) {
+                $uuid = uniqid("", true);
+                update_token($email, $uuid);
+                $user_email = $email;  // Replace with actual data retrieval
+                send_login_retrial_link($user_email, "https://tekon.co.zw/client/api/activate_account.php?token=$uuid");
+                // Optionally, lock the account or display a message to the user
+                $err = "Your account have been temporary banned, you have reached the number of trials, Please check your email, we have sent your the activation code";
+            } else {
+                $err = "Access Denied Please Check Your Credentials,you will be redirected back to login page";
+            }
         } else {
             $err = "Access Denied Please Check Your Credentials,you will be redirected back to login page";
-        ?>
-            <script>
-                setTimeout(() => {
-                    window.location.href = "/client/pages_client_index.php";
-                }, 2000)
-            </script>
-<?php
         }
+        ?>
+        <script>
+            setTimeout(() => {
+                window.location.href = "/client/pages_client_index.php";
+            }, 2000)
+        </script>
+<?php
+
     }
 }
